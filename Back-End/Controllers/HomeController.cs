39d27de5dd181c11f7734bea53ec4c641ada07a0
Sefiyye <tu7l1lxat@code.edu.az>
2022-05-22 17:45:26 +0400

    task

---
+++ Back-End/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Back_End.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int id)
+        {
+            ViewBag.Product = "Cola";
+            ViewData["Count"] = id;
+            TempData["Type"] = "Cold";
+            return RedirectToAction("About");
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult FAQ(string name, string surname)
+        {
+            string fullname = $"{name} {surname}";
+            return View(fullname);
+        }
+    }
+}
